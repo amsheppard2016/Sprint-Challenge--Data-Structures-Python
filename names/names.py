ad286288b480a13eaa647a3331a929b337a9a122
Ash W <amsheppard2016@gmail.com>
2020-09-28 23:27:22 -0400

    sprint done

--- names/names.py
+++ names/names.py
@@ -1,3 +1,75 @@
+class BinarySearchTree:
+    def __init__(self):
+        self.root_node = None
+    
+    def isEmpty(self):
+        return self.root_node is None
+    
+    def insert(self, value):
+        if self.isEmpty():
+            self.root_node = BSTNode(value)
+        else:
+            self.root_node.insert(value)
+    
+    def contains(self, target):
+        if self.isEmpty():
+            return False
+        else:
+            return self.root_node.contains(target)
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+            
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        if self.right == None:
+            max_value = self.value
+            return max_value
+        return self.right.get_max()
+    
+
+    # Call the function `fn` on the value of each node
+    def for_each(self, fn):
+        fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
 import time
 
 start_time = time.time()
@@ -10,13 +82,23 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
+duplicates = []  # Return the list of duplicates in this data structure
+bst=BinarySearchTree()
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    bst.insert(name_1)
+
+for name_2 in names_2:
+    if bst.contains(name_2):
+        duplicates.append(name_2)
+# Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -26,3 +108,5 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# duplicates = (set(names_1) & set(names_2))
